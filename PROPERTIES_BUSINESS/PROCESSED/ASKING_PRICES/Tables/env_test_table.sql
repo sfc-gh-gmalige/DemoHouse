4eb06cc906eefc539dbc82be5328f80df9a91430
gmalige <gokula.malige@snowflake.com>
2022-06-27 15:50:06 +0100

    test script

--- PROPERTIES_BUSINESS/PROCESSED/ASKING_PRICES/Tables/env_test_table.sql
+++ PROPERTIES_BUSINESS/PROCESSED/ASKING_PRICES/Tables/env_test_table.sql
@@ -1,4 +1,4 @@
 -- liquibase formatted sql
 
 -- changeset GMALIGE:1 
-CREATE TABLE DEMO.CICD_LOGS.DEMO1 (test_id INT, test_column VARCHAR, PRIMARY KEY (test_id));
+CREATE TABLE DEMO.CICD_LOGS.DEMO1 (test_id INT, test_column VARCHAR, load_date date, PRIMARY KEY (test_id));
